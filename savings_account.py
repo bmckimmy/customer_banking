f2e9140b4fc2092e5f83b41380a677b4ec89c4db
bmckimmy <145221576+bmckimmy@users.noreply.github.com>
2023-11-13 07:29:14 -0700

    updated

--- savings_account.py
+++ savings_account.py
@@ -22,7 +22,11 @@ def create_savings_account(balance, interest_rate, months):
 
     # Update the savings account balance by adding the interest earned
     new_balance = balance + interest_earned
- 
+    savings_account.set_balance(new_balance)
+
+    # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
+    savings_account.set_balance(new_balance)
+
     # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
     savings_account.set_interest(interest_earned)
 
